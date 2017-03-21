d47083496f3adf346154f909a467155ec7d71a51
juandgaines <jdavid90.2@gmail.com>
2017-03-21 15:27:15 -0500

    feat: News App Creation
    We created the news app project stating with the New class. we added the attrbutes : Title, publication date, Section name and url.
    
    Resolves: none
    See also: none

---
+++ app/src/main/java/com/mytechideas/newsapp/News.java
@@ -0,0 +1,37 @@
+package com.mytechideas.newsapp;
+
+/**
+ * Created by JuanDavid on 20/03/2017.
+ */
+
+public class News {
+
+    private String mPublicationDate;
+    private String mUrl;
+    private String mTitle;
+    private String mSectionName;
+
+
+    public News(String sectionName,String Title, String publicationDate, String url){
+
+        mTitle=Title;
+        mPublicationDate=publicationDate;
+        mSectionName=sectionName;
+        mUrl=url;
+
+    }
+
+    public String getPublicationDate(){
+        return mPublicationDate;
+    }
+    public String getUrl(){
+        return mUrl;
+
+    }
+    public String getSectionName(){
+        return mSectionName;
+    }
+    public String getTitle(){
+        return mTitle;
+    }
+}
