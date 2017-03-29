b4d59183a2442e1eeb0d0e854aed1abb4922d5d5
juandgaines <jdavid90.2@gmail.com>
2017-03-28 19:21:16 -0500

    feat: fetching data from internet throught AsyncTaskLoader
    We added static functions to the QueryUtilis.java to fetch data from internet calling Url classes. For the previous actions we have to add internet permssion and network state permissions to the Android manifest and implemented the callback methods for Loader through a new class called NewLoader thar extends AsyncTaskLoader.
    
    Resolves: None
    See also: None
    
    -modified:   app/src/main/AndroidManifest.xml
    -modified:   app/src/main/java/com/mytechideas/newsapp/MainActivity.java
    -new file:   app/src/main/java/com/mytechideas/newsapp/NewsLoader.java
    -modified:   app/src/main/java/com/mytechideas/newsapp/QueryUtils.java

---
+++ app/src/main/java/com/mytechideas/newsapp/NewsLoader.java
@@ -0,0 +1,32 @@
+package com.mytechideas.newsapp;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by JuanDavid on 28/03/2017.
+ */
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    private String mUrl;
+    public NewsLoader(Context context, String url) {
+        super(context);
+        this.mUrl=url;
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+        if(mUrl==null)
+            return  null;
+
+        List<News> list=QueryUtils.fetchNewsData(mUrl);
+        return list;
+    }
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+}
--- app/src/main/java/com/mytechideas/newsapp/QueryUtils.java
+++ app/src/main/java/com/mytechideas/newsapp/QueryUtils.java
@@ -6,13 +6,23 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by JuanDavid on 21/03/2017.
  */
 
 public class QueryUtils {
+    public static final String LOG_TAG = QueryUtils.class.getSimpleName();
 
     public static ArrayList<News> extractNewsFromJSON(String jsonString) {
         ArrayList<News> news = new ArrayList<News>();
@@ -42,4 +52,91 @@ public class QueryUtils {
     }
 
 
+    public static List<News> fetchNewsData(String requestUrl) {
+        // Create URL object
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error closing input stream", e);
+        }
+
+        // Extract relevant fields from the JSON response and create an {@link Event} object
+        List<News> earthquake = extractNewsFromJSON(jsonResponse);
+
+        // Return the {@link Event}
+        return earthquake;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL ", e);
+        }
+        return url;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
 }
